05cb36c7e07009711bcef3c93c8d8ed17899912f
soso221 <steal219@naver.com>
2024-01-12 16:43:19 +0900

    fix : change order:

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,11 +1,11 @@
 def do_fizzbuzz():
     for i in range(1, 15+1):
+        elif i%15==0:
+            print('fizzbuzz')
         if i%3==0:
             print('fizz')
         elif i%5==0:
             print('buzz')
-        elif i%15==0:
-            print('fizzbuzz)
         else:
             print(f'{i}')
 
